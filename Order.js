dcffae78738c70467beaa5acb50dc1588ef9bc25
vaibhavantil <vaibhav.antil@gmail.com>
2021-07-20 21:33:17 +0530

    Create Order.js

---
+++ Order.js
@@ -0,0 +1,7 @@
+    CLOSE_WINDOW: 'close-window',
+        CLOSE_LIGHTBOX: 'close-lightbox',
+        STATUS: 'status',
+        STATUS_INVOICE: 'status-invoice',
+        STATUS_DELIVERING: 'status-delivering',
+        STATUS_TROUBLED: 'status-troubled',
+        STATUS_DONE: 'status-done',
